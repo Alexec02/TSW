82dcf00b35c64c25900be90c80e5bb745b0350bd
civeira <93115179+civeira@users.noreply.github.com>
2023-11-05 22:13:31 +0100

    Add files via upload

--- lampserver/my-web-project/www/view/layouts/default.php
+++ lampserver/my-web-project/www/view/layouts/default.php
@@ -10,8 +10,10 @@ $currentuser = $view->getVariable("currentusername");
 	<title><?= $view->getVariable("title", "no title") ?></title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href= "css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<!-- enable ji18n() javascript function to translate inside your scripts -->
 	<script src="index.php?controller=language&amp;action=i18njs">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	</script>
 	<?= $view->getFragment("css") ?>
 	<?= $view->getFragment("javascript") ?>
