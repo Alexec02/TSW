9a54268b7a4800f75024b3837dd4a3f8747068a2
civeira <93115179+civeira@users.noreply.github.com>
2023-11-05 20:29:00 +0100

    Add files via upload

--- lampserver/my-web-project/www/view/switchs/add.php
+++ lampserver/my-web-project/www/view/switchs/add.php
@@ -9,17 +9,25 @@ $errors = $view->getVariable("errors");
 $view->setVariable("title", "Edit Switch");
 
 ?>
-<link rel="stylesheet" type="text/css" href="nuevoSwitch.css">
+<link rel="stylesheet" href="css/nuevoSwitch.css" type="text/css">
 
 <h1><?= i18n("Create switch")?></h1>
 <form action="index.php?controller=switchs&amp;action=add" method="POST">
-	<?= i18n("Name") ?>: <input type="text" name="nombre"
-	value="<?= $switch->getNombre() ?>">
-	<?= isset($errors["nombre"])?i18n($errors["nombre"]):"" ?><br>
-	<?= i18n("Description") ?>: <input type="text" name="descripcion"
-	value="<?= $switch->getDescripcion() ?>">
-	<?= isset($errors["descripcion"])?i18n($errors["descripcion"]):"" ?><br>
+    <div class="form-container">
+        <div class="form-group">
+            <label for="nombre"><?= i18n("Name") ?>:</label>
+            <input type="text" name="nombre" id="nombre" value="<?= $switch->getNombre() ?>">
+            <?= isset($errors["nombre"]) ? i18n($errors["nombre"]) : "" ?>
+        </div>
 
-	<input type="submit" name="submit" value="<?= i18n("Submit") ?>">
-	
+        <div class="form-group">
+            <label for="descripcion"><?= i18n("Description") ?>:</label>
+            <input type="text" name="descripcion" id="descripcion" value="<?= $switch->getDescripcion() ?>">
+            <?= isset($errors["descripcion"]) ? i18n($errors["descripcion"]) : "" ?>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" name="submit" value="<?= i18n("Submit") ?>">
+        </div>
+    </div>
 </form>
