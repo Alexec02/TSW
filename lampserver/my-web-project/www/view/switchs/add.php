42af9ebef5d2f95df8314df4274ba1dde0e05524
civeira <93115179+civeira@users.noreply.github.com>
2023-11-05 20:21:18 +0100

    Add files via upload

--- lampserver/my-web-project/www/view/switchs/add.php
+++ lampserver/my-web-project/www/view/switchs/add.php
@@ -8,14 +8,18 @@ $errors = $view->getVariable("errors");
 
 $view->setVariable("title", "Edit Switch");
 
-?><h1><?= i18n("Create switch")?></h1>
+?>
+<link rel="stylesheet" type="text/css" href="nuevoSwitch.css">
+
+<h1><?= i18n("Create switch")?></h1>
 <form action="index.php?controller=switchs&amp;action=add" method="POST">
-	<?= i18n("name") ?>: <input type="text" name="nombre"
+	<?= i18n("Name") ?>: <input type="text" name="nombre"
 	value="<?= $switch->getNombre() ?>">
 	<?= isset($errors["nombre"])?i18n($errors["nombre"]):"" ?><br>
-	<?= i18n("description") ?>: <input type="text" name="descripcion"
+	<?= i18n("Description") ?>: <input type="text" name="descripcion"
 	value="<?= $switch->getDescripcion() ?>">
 	<?= isset($errors["descripcion"])?i18n($errors["descripcion"]):"" ?><br>
 
-	<input type="submit" name="submit" value="submit">
+	<input type="submit" name="submit" value="<?= i18n("Submit") ?>">
+	
 </form>
