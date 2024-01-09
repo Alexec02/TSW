2a60e8420325140752a275d75c01488f364ee505
Alexec02 <83095538+Alexec02@users.noreply.github.com>
2024-01-09 21:45:05 +0100

    Switch index

--- lampserver/my-web-project/www/templates/helpers.js
+++ lampserver/my-web-project/www/templates/helpers.js
@@ -4,3 +4,10 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
   else
     return opts.inverse(this);
 });
+Handlebars.registerHelper('eq', function (value1, value2, options) {
+  if (value1 === value2) {
+      return true;
+  } else {
+      return false;
+  }
+});
